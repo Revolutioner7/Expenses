794a85595fae3371760abffb903c26fccd327831
Revolutioner7 <rodrigoharmat@gmail.com>
2026-09-06 12:13:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 /* Dineriko — funciona sin conexión */
-const CACHE = 'cosecha-v25';
+const CACHE = 'cosecha-v29';
 const SHELL = [
   './',
   './index.html',
-  './bundle.js?v=25',
-  './styles.css?v=25',
+  './bundle.js?v=29',
+  './styles.css?v=29',
   './manifest.webmanifest',
   './icon-192.png',
   './icon-512.png',
